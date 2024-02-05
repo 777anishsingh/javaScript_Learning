86a6bb2cf4b36cfe90b65899c3972ac5873db9d4
Anish Singh Butola <95209172+777anishsingh@users.noreply.github.com>
2024-02-05 19:41:36 +0000

    COMPARISONS

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("02" > 1);
+// console.log("02" > 1);
+
+
+//************* CONVERSIONS THAT WE USUALLY DONT PREFFER ***************/
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); 
+
+//value conversion problem as compariosn and equality check operation works differently in javascript
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+// console.log(undefined != 0);
+
+// === strict check =>check values and its datatype
+console.log("2"===2);
+
+
+
+
+
+
