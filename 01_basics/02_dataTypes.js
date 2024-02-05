ba0364b20688a4dd4b6bf846329b79837afccd86
Anish Singh Butola <95209172+777anishsingh@users.noreply.github.com>
2024-02-05 18:54:29 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all Js as newer version
+// alert("3+3") // we are using nodeJs not browser
+
+let name  = "Anish" //string
+let age = 18 //number
+let isLoggedIn = false //bool
+
+//bigint for big numbers
+//string => ""
+//boolean => true/false
+// null => standalone value also it is of type Object
+// undefined => value not assigned yet
+//symbol => to find unique
+
+//object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
