4782273239581faf70ea8ea7eca50d481795d5f5
Anish Singh Butola <95209172+777anishsingh@users.noreply.github.com>
2024-02-05 19:30:05 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,57 @@
+// let score = "Anish"
+
+// console.log(typeof score);
+
+/***************************** NUMBER *********************************** */
+// let valueInNumber = Number(score) // gurantee that score is in number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+/***************************** BOOLEAN *********************************** */
+// let isLoggedIn = "Anish"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 =>false
+// ""=>false; "any thing"=> true
+
+/***************************** STRING *********************************** */
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+///******************************** OPERATIONS ******************************************////
+
+// let value = 3
+// let negVal = -value
+// console.log(negVal);
+
+// console.table([2+2,2-2,2*2,2**3,2/2,2%2]); // ** => power
+
+// let str1 = "Hello"
+// let str2 = " Anish"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+console.log(gameCounter++);
+console.log(++gameCounter);
+
