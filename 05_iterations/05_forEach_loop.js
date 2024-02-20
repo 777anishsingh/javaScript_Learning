e95460dd32c06b3eb03209adaf5eb0c842d43bf7
777anishsingh <777anish.singh@gmail.com>
2024-02-20 21:17:56 +0530

    update_05_iterators

--- 05_iterations/05_forEach_loop.js
+++ 05_iterations/05_forEach_loop.js
@@ -4,21 +4,20 @@
 const coding = ['js', 'ruby', 'java', 'python', 'cpp']
 
 // 1st way
-// coding.forEach(function (itr) {
-//     console.log(itr);
-// })
+coding.forEach(function (itr) {
+    // console.log(itr);
+})
 
 // 2nd way
-// coding.forEach((itr) => {
-//     console.log(itr);
-// })
+coding.forEach((itr) => {
+    // console.log(itr);
+})
 
 // 3rd way
-// function printMe(item){
-//     console.log(item);
-// }
-
-// coding.forEach(printMe)
+function printMe(item){
+    // console.log(item);
+}
+coding.forEach(printMe)
 
 // 4th way
 coding.forEach((itr, index, coding) => {
@@ -42,8 +41,8 @@ const myCoding = [
 ]
 
 myCoding.forEach((item) => {
-    console.log(item.fileName);
-    console.log(item.langNmae);
+    // console.log(item.fileName);
+    // console.log(item.langNmae);
     // console.log(item.coding);
 })
 
